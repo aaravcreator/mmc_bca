d7ebe6947342fd129d137021de04e282a28a627a
Aarav Poudel <poudelaarav@gmail.com>
2024-08-08 16:23:33 +0545

    crud

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,4 +5,7 @@ from .views import *
 urlpatterns = [
     path('list_student/',list_student_view),
     path('create/',create_student),
+    path('update/<int:id>/',update_student),
+    path('delete/<int:id>/',delete_student),
+    #localhost:8000/update/1/
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Student
 from .forms import StudentForm
 # Create your views here.
@@ -10,10 +10,40 @@ def list_student_view(request):
 
     return render(request,"list_student.html",data)
 
+def delete_student(request,id):
+    student = Student.objects.get(id=id)
+
+    student.delete()
+    return redirect('/list_student/')
+
+def update_student(request,id):
+    print(id)
+    student = Student.objects.get(id=id)
+    form = StudentForm(instance=student)
+    if request.method == "POST":
+        form = StudentForm(request.POST,instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('/list_student/')
+    data = {
+        'form':form
+    }
+
+    return render(request,'update.html',data)
+
+
 def create_student(request):
 
     print(request.POST)
     form = StudentForm()
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save() # saves the student data to database
+            return redirect('/list_student/')
+
+
+    
     data = {
         'form':form
     }
